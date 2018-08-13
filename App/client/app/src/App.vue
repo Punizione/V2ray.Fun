0f9ac856bcddc04b1425974457ae51041e59d7cb
Punizione <1273435147@qq.com>
2018-08-13 21:10:31 +0800

    update

--- App/client/app/src/App.vue
+++ App/client/app/src/App.vue
@@ -1,18 +1,10 @@
 <template>
   <v-app>
-    <v-navigation-drawer
-      persistent
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      v-model="drawer"
-      enable-resize-watcher
-      fixed
-      app
-    >
+    <v-navigation-drawer fixed clipped app v-model="drawer" floating>
       <v-list>
         <v-list-tile
           value="true"
-          v-for="(item, i) in items"
+          v-for="(item, i) in drawItems"
           :key="i"
         >
           <v-list-tile-action>
@@ -24,47 +16,27 @@
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-    <v-toolbar
-      app
-      :clipped-left="clipped"
+    <v-toolbar 
+      color="blue darken-3" 
+      dark 
+      app 
+      :clipped-left="$vuetify.breakpoint.mdAndUp" 
+      fixed
     >
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>remove</v-icon>
-      </v-btn>
       <v-toolbar-title v-text="title"></v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
+      <v-btn icon >
         <v-icon>menu</v-icon>
       </v-btn>
     </v-toolbar>
     <v-content>
-      <router-view/>
+      <v-fade-transition mode="out-in">
+        <router-view/>
+      </v-fade-transition>
     </v-content>
-    <v-navigation-drawer
-      temporary
-      :right="right"
-      v-model="rightDrawer"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile @click="right = !right">
-          <v-list-tile-action>
-            <v-icon>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
     <v-footer :fixed="fixed" app>
-      <span>&copy; 2017</span>
+      <span>&copy; 2018</span>
     </v-footer>
   </v-app>
 </template>
@@ -73,17 +45,13 @@
 export default {
   data () {
     return {
-      clipped: false,
       drawer: true,
       fixed: false,
-      items: [{
+      drawItems: [{
         icon: 'bubble_chart',
         title: 'Inspire'
       }],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'V2ray.Fun'
     }
   },
   name: 'App'
